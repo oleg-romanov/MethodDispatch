584be67914400b2efc6d70667c62672854d5f99d
oleg-romanov <55031494+oleg-romanov@users.noreply.github.com>
2021-09-20 19:19:27 +0300

    added static dispatch examples

---
+++ MethodDispatch/MethodDispatch/StaticDispatch/SomeClass.swift
@@ -0,0 +1,14 @@
+//
+//  SomeClass.swift
+//  MethodDispatch
+//
+//  Created by Олег Романов on 9/20/21.
+//
+
+import Foundation
+
+final class SomeClass {
+    func coolFunc() {
+        print("coolFunc")
+    }
+}
